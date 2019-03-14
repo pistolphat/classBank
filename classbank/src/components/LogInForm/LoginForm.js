bdb33d0262a6f5f99ef1eff6d160861f2e94e081
Phat Doan <phatdoan@yahoo.com>
2019-03-14 14:57:36 -0400

    logout Redirect to Login OK

--- classbank/src/components/LogInForm/LoginForm.js
+++ classbank/src/components/LogInForm/LoginForm.js
@@ -10,18 +10,23 @@ class LogInForm extends Component {
   render() {
     return (
       <div className='container bg-light text-dark mt-5 py-3 rounded' style={{maxWidth: "400px"}}>
+
         <h1>Log In</h1>
+        <br />
         <form>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
             <input type="text" name="email" onChange={this.props.handleInput} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email address"/>
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            
           </div>
+
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Password</label>
             <input type="password" name="password"onChange={this.props.handleInput} className="form-control" id="exampleInputPassword1" placeholder="Password"/>
           </div>
+
           <input className="btn btn-primary" value="Submit" type="submit" onClick={this.props.handleLogIn}/>
+
         </form>
           {this.props.isLoggedIn ? this.redirect(): null}
       </div>
--- classbank/src/components/Logout/Logout.js
+++ classbank/src/components/Logout/Logout.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 // import { Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import "./Logout.css";
 
 class LogOut extends Component {
 
   render() {
     return (
-      <div>
+
+        <div>
         <h2>Log Out</h2>
+
         <form>
-          <input
-            className="btn btn-primary"
-            value="Log Out"
-            type="submit"
-            onClick={this.props.handleLogOut}
-          />
+          <Link to='/login'><input className="btn btn-primary" value="Log Out" type="submit" onClick={this.props.handleLogOut} /></Link>
         </form>
+
       </div>
     );
   }
